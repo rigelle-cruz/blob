1c8136038f8787c338aa8b949d615532bbc451d4
Rigelle Cruz <rigellecruz@gmail.com>
2023-10-16 11:49:17 +1300

    make mouse event to trigger animation

--- client/components/Blobs.tsx
+++ client/components/Blobs.tsx
@@ -1,3 +1,51 @@
+import { useState } from 'react';
+
 export const Blobs = () => {
-	return <div>Blobs</div>;
+	const [isAnimating, setIsAnimating] = useState(false);
+
+	const handleMouseDown = () => {
+		setIsAnimating(true);
+	};
+
+	const handleMouseUp = () => {
+		setIsAnimating(false);
+	};
+
+	const ballClass = isAnimating ? 'ball animate' : 'ball';
+
+	return (
+		<>
+			<div className="wrapper">
+				<div
+					onMouseDown={handleMouseDown}
+					onMouseUp={handleMouseUp}></div>
+				<div
+					className={ballClass}
+					onMouseDown={handleMouseDown}
+					onMouseUp={handleMouseUp}></div>
+				<div
+					className={ballClass}
+					onMouseDown={handleMouseDown}
+					onMouseUp={handleMouseUp}></div>
+			</div>
+			<svg>
+				<defs>
+					<filter id="filter">
+						<feGaussianBlur
+							in="SourceGraphic"
+							stdDeviation="18"
+							result="blur"
+						/>
+						<feColorMatrix
+							in="blur"
+							mode="matrix"
+							values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 28 -10"
+							result="filter"
+						/>
+						<feComposite in="SourceGraphic" in2="filter" operator="atop" />
+					</filter>
+				</defs>
+			</svg>
+		</>
+	);
 };
